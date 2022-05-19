ec53f995f83d676e367024745f2c0a54cb86287f
shgur7236 <shgurtns7236@naver.com>
2022-05-19 11:40:36 +0900

    JPA Auditing을 사용하여 생성시간과 수정시간 등록 구현

--- src/main/java/board/myboard/MyboardApplication.java
+++ src/main/java/board/myboard/MyboardApplication.java
@@ -2,7 +2,9 @@ package board.myboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // Auditing을 사용할려면 @EnableJpaAuditing을 꼭 적어야 한다.
 @SpringBootApplication
 public class MyboardApplication {
 
--- src/main/java/board/myboard/domain/BaseTimeEntity.java
+++ src/main/java/board/myboard/domain/BaseTimeEntity.java
@@ -1,4 +1,28 @@
 package board.myboard.domain;
 
-public class BaseTimeEntity {
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(updatable = true)
+    private LocalDate lastModifiedDate;
 }
