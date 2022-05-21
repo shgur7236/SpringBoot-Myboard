f71afbf4be544ff2f83852ffae5f7ed65b51edc9
shgur7236 <shgurtns7236@naver.com>
2022-05-21 18:02:50 +0900

    SimpleUrlAuthenticationFailureHandler를 상속받아 LoginFailureHandler 작성

--- src/main/java/board/myboard/global/login/handler/LoginFailureHandler.java
+++ src/main/java/board/myboard/global/login/handler/LoginFailureHandler.java
@@ -1,4 +1,22 @@
 package board.myboard.global.login.handler;
 
-public class LoginFailureHandler {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException authentication) throws IOException, ServletException{
+        response.setStatus(HttpServletResponse.SC_OK); // 보안을 위해 로그인 오류지만 200반환
+        response.getWriter().write("fail");
+        log.info("로그인에 실패했습니다.");
+    }
 }
