17d00441c5db40ac1419c67d23600b20414f5942
shgur7236 <shgurtns7236@naver.com>
2022-05-23 09:16:52 +0900

    기본 로그인 테스트 형식

---
+++ src/test/java/board/myboard/global/login/LoginTest.java
@@ -0,0 +1,86 @@
+package board.myboard.global.login;
+
+import board.myboard.domain.member.Member;
+import board.myboard.domain.member.Role;
+import board.myboard.domain.member.repository.MemberRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.HashMap;
+import java.util.Map;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    PasswordEncoder delegatingPasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    private static String KEY_USERNAME = "username";
+    private static String KEY_PASSWORD = "password";
+    private static String USERNAME = "username";
+    private static String PASSWORD = "123456789";
+
+    private static String LOGIN_URL = "/login";
+
+    @Value("${jwt.access.header}")
+    private String accessHeader;
+
+    @Value("${jwt.refresh.header}")
+    private String refreshHeader;
+
+    private void clear(){
+        em.flush();
+        em.clear();
+    }
+
+    @BeforeEach
+    private void init(){
+        memberRepository.save(Member.builder()
+                .username(USERNAME)
+                .password(delegatingPasswordEncoder.encode(PASSWORD))
+                .name("Member1")
+                .nickName("NickName1")
+                .role(Role.USER)
+                .age(22)
+                .build());
+        clear();
+    }
+
+    private Map getUsernamePasswordMap(String username, String password){
+        Map<String, String> map = new HashMap<>();
+        map.put(KEY_USERNAME, username);
+        map.put(KEY_PASSWORD, password);
+        return map;
+    }
+
+    private ResultActions perform(String url, MediaType mediaType, Map usernamePasswordMap) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders
+                .post(url)
+                .contentType(mediaType)
+                .content(objectMapper.writeValueAsString(usernamePasswordMap)));
+    }
+}
